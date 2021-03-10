7fabe2de4d0ac31906cab18d73a7e4111479f65d
lukrita <rluk3@gatech.edu>
2021-03-10 15:07:22 -0800

    added App and index js files

---
+++ Viz/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Simple from './Simple';
+import BarChart from './BarChart';
+
+const App = () => {
+    return (
+        <h1>
+            Hello
+        </h1>
+    )
+}
+
+export default App;
