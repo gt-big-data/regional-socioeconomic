4a72b109f1927326ae90b6ff1e8aaefe30b8b5a6
rluk3 <rluk3@gatech.edu>
2021-03-12 15:08:34 -0800

    updated with dropdown plus title

--- Viz/project/src/App.jsx
+++ Viz/project/src/App.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import DropDownClass from "./components/DropDownClass";
 import { Container } from "semantic-ui-react";
 import {countries} from "./dataFiles/countries";
+import index from './index.css'
 
 const App = () => {
     return (
+        
         <Container>
+            <div className = "Title"> 
+                <h1>Regional Socioeconomic Project</h1>
+            </div>
             <DropDownClass />
         </Container>
     )
