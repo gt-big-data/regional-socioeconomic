1ace4d51b2ce65ca1ced7ba7ca8ea2f880eb9e2f
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2021-03-20 16:35:39 -0400

    add country_chosen state

--- Viz/project/src/components/DropDownClass.js
+++ Viz/project/src/components/DropDownClass.js
@@ -5,8 +5,17 @@ import countries from "../dataFiles/countries";
 class DropDownClass extends Component {
     constructor () {
         super();
-        this.state = {}
-    }  
+        this.state = {
+            country_chosen: "none"
+        }
+        
+    } 
+    
+    change_country = (e, data) => {
+        var country_name = 
+        this.setState({country_chosen: data.value});
+        console.log(data);
+    }
 
     render() {
         const menuStyle = {
@@ -17,12 +26,14 @@ class DropDownClass extends Component {
         return(
             <>
                 <Dropdown
+                    onChange = {this.change_country}
                     style = {menuStyle}
                     placeholder='Select Country'
                     fluid
                     selection
                     options={countries}
                 />
+                <p>{`Current Country: ${this.state.country_chosen}`}</p>
             </>
         );
     }
