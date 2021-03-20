a09ac3d37f316f28dcbf7132db667323220d11a3
rluk3 <rluk3@gatech.edu>
2021-03-20 13:52:04 -0700

    svg attempt

--- Viz/project/src/App.jsx
+++ Viz/project/src/App.jsx
@@ -3,6 +3,7 @@ import DropDownClass from "./components/DropDownClass";
 import { Container } from "semantic-ui-react";
 import {countries} from "./dataFiles/countries";
 import index from './index.css'
+import mapDisplay from './components/mapDisplay';
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
                 <h1>Regional Socioeconomic Project</h1>
             </div>
             <DropDownClass />
+            <mapDisplay />
         </Container>
     )
 }
---
+++ Viz/project/src/components/mapDisplay.js
@@ -0,0 +1,10 @@
+import ReactLogo from './us.svg';
+
+const mapDisplay = () => {
+    return (
+      <div className="Map">
+        <img src={ReactLogo} alt="US Map" />
+      </div>
+    );
+  }
+  export default mapDisplay;
