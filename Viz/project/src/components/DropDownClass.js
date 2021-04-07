e9570729efd783970d8bbe971c88d8510d9330f8
Pan, Yu <yupan@gatech.edu>
2021-04-07 00:22:53 -0400

    Fix bug in DropDown

--- Viz/project/src/components/DropDownClass.js
+++ Viz/project/src/components/DropDownClass.js
@@ -6,7 +6,7 @@ class DropDownClass extends Component {
     constructor () {
         super();
         this.state = {
-            country_chosen = ""
+            country_chosen: ""
         }
         
         
