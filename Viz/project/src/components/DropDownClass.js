dc232d1796bcc33a40fe68d8bd56f3ae5286a6a2
lukrita <67081095+lukrita@users.noreply.github.com>
2021-04-09 21:15:48 -0700

    Added working buttons and text

--- Viz/project/src/components/DropDownClass.js
+++ Viz/project/src/components/DropDownClass.js
@@ -1,22 +1,71 @@
-
 import React, {Component} from 'react'
 import { Dropdown } from 'semantic-ui-react'
 import countries from "../dataFiles/modifiedCountries";
 import Venezuela from "./images/Antarctica/vector.svg";
+import index from '../index.css';
 
 class DropDownClass extends Component {
+
     constructor () {
         super();
         this.state = {
-            country_chosen: require("./images/Antarctica/vector.svg")
+            country_chosen: require("./images/Antarctica/vector.svg"),
+            country_string: "/Antarctica/vector.svg",
+            country_name: "Antarctica"
         }
-        
-        
     } 
+
+    Next_Click = (e) =>  {
+        var current = 0;
+        for (var i = 0; i < countries.length; i++) {
+            if ((countries[i].value) == (this.state.country_string)) {
+                current = countries[i].num;
+                break;
+            }
+        }
+        console.log(current );
+        var imagePath1 = require("./images" + countries[current].value);
+        this.setState({country_chosen: imagePath1});
+        this.setState({country_string: countries[current].value});
+        this.setState({country_name: countries[current].text});
+        console.log(this.state.country_string);
+        //console.log()
+    }
+
+    Previous_Click = (e) =>  {
+        var current = 0;
+        for (var i = 0; i < countries.length; i++) {
+            if ((countries[i].value) == (this.state.country_string)) {
+                current = countries[i].num;
+                break;
+            }
+        }
+        console.log(current);
+        current-=2;
+        console.log(current);
+        console.log(countries[current].value);
+        var imagePath1 = require("./images" + countries[current].value);
+        this.setState({country_chosen: imagePath1});
+        this.setState({country_string: countries[current].value});
+        this.setState({country_name: countries[current].text});
+        console.log(this.state.country_string);
+        //console.log()
+    }
     
     change_country = (e, data) => {
         var imagePath = require("./images" + data.value)
         this.setState({country_chosen: imagePath});
+        this.setState({country_string: data.value});
+        var current = 0;
+        for (var i = 0; i < countries.length; i++) {
+            if ((countries[i].value) == (this.state.country_string)) {
+                current = countries[i].num;
+                break;
+            }
+        }
+        this.setState({country_name: countries[current-2].text});
+        //console.log(this.state.country_chosen);
+        console.log(data);
     }
 
     do_nothing = (e) => {
@@ -30,6 +79,7 @@ class DropDownClass extends Component {
 
         return(
             <>
+            <p> Click to Choose Country </p>
                 <Dropdown
                     onChange = {this.change_country}
                     style = {menuStyle}
@@ -38,7 +88,16 @@ class DropDownClass extends Component {
                     selection
                     options={countries}
                 />
-                <img src={this.state.country_chosen.default} alt="not found"/>
+                <p> Current Country : {this.state.country_name}</p>
+                <img src={this.state.country_chosen.default} alt="not found" width="400" height="400"/>
+                <a href="#" onClick={this.Previous_Click}>
+                    Previous
+                </a>
+                <p>                </p>
+                <a href="#" onClick={this.Next_Click}>
+                    Next
+                </a>
+                
             </>
         );
     }
