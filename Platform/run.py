487ba4e016a414ad7ec138e88cd463c108b08534
Stephen Buckley <sbuckley@gatech.edu>
2021-10-31 16:09:14 -0400

    Add training images

--- Platform/run.py
+++ Platform/run.py
@@ -4,7 +4,7 @@ import get_basic_images as gbi
 #An array of all queries that need to be run 
 #This is a google search query so search syntax can be used link below for examples
 #https://support.google.com/websearch/answer/2466433?hl=en
-queries = ["Rivers"]
+queries = ["Villages", "Roads", "Cities", "Rivers"]
 #Loop through the queries array and run the query, save response arrray to results
 #Use results to extract image links and download_image_from_url to download 
 for query in queries:
@@ -12,4 +12,4 @@ for query in queries:
     links = gbi.get_image_links_from_response(results)
     for link in links:
         #save the images to images directory and in seperated folders by query
-        gbi.download_image_from_url(link, "images2/" + query + "/" + query + str(links.index(link)))
+        gbi.download_image_from_url(link, "images2/" + query + "/" + query + str(links.index(link)) + ".png")
